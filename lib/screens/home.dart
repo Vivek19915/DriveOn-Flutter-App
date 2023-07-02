f8200001a841a45e119d601111978e1d82570295
Vivek19915 <vchoudhary19915@gmail.com>
2023-07-02 17:25:44 +0530

    google maps setup in app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,16 +1,26 @@
 import 'package:driveon_flutter_app/widgets/bg_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return bgWidget(
-      child: Scaffold(
-        body: "logged in".text.makeCentered(),
-      ),
+    return  Scaffold(
+        body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+
+          },
+        ),
     );
   }
 }
